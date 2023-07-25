775212be304749865d32cd9e62f40d2fb28d32b8
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-25 02:09:30 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
