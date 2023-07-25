322ba64ab3b367e74df5f574d2e7e9603cd4716f
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-25 07:35:20 -0700

    created print_string

---
+++ print_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int print_string (char *str)
+{
+  int i=0;
+
+  if (str == NULL)
+    {
+      _putchar('(');
+      _putchar('n');
+      _putchar('u');
+      _putchar('l');
+      _putchar('l');
+      _putchar(')');
+    }
+  while (str[i] )
+ 
+    {
+      putchar(str[i++]);
+ 
+    }
+  return (str);
+}
