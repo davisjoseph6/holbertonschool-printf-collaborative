5bb419fc8779c871adad0f374b4e6cb5a2f33cd3
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-26 07:55:00 -0700

    main.c

--- main.c
+++ main.c
@@ -16,8 +16,9 @@ int main(void)
 
   setbuf(stdout, 0);
 
-  len = _printf("welcome to holberton school.\n");
-  len2 = printf("welcome to holberton school.\n");
+
+  len = _printf("Let's try to printf a simple sentence.\n");
+  len2 = printf("Let's try to printf a simple sentence.\n");
   ui = (unsigned int)INT_MAX + 1024;
   addr = (void *)0x7ffe637541f0;
   _printf("Length:[%d, %i]\n", len, len);
