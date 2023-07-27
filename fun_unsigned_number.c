c306a910df24842050009ed9f77110e52b0f5bfa
Davis <davisjoseph767@gmail.com>
2023-07-27 16:20:53 -0700

    fun_unsigned_number.c

--- fun_unsigned_number.c
+++ fun_unsigned_number.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * 
+ * 
+ *
+ * 
+ * 
+ */
+
 int len_unsigned_number(unsigned int num)
 {
 	int count = 0;
@@ -16,6 +24,14 @@ int len_unsigned_number(unsigned int num)
 
 	return (count);
 }
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 
 void fun_unsigned_number(unsigned int num)
 {
