b6f022388929dd1b8038a94d6d1be19d2ed6f20c
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-27 02:50:08 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "main.h"
+
 
 
 
@@ -12,47 +12,63 @@ int _printf(const char *format, ...)
   int count = 0;
   va_list args;
   va_start(args, format);
-  if (!format || !format[0]) 
-        return -1;
+  if (!format || !format[0])
+    return -1;
   while (*format != '\0')
-        {
+    {
       if (*format != '%')
-       {
-       count += _putchar(*format);
+	{
+	  count += _putchar(*format);
 
-      }
+	}
       else
-      {
-     format++; 
-               if (*format == 'c')
-               {
-               char c = va_arg(args, int);
-               _putchar(c);
-                count++;
-               }
-               else if (*format == 's')
-               {
-               char *str = va_arg(args, char *);
-               count += print_string(str);
-              }
-              else if  (*format == '%')
-              {
-               
-              count += _putchar(*format);
-              }
-              else if (*format == 'd' || *format == 'i')
-              {
-                {
-	          int num = va_arg(args, int);
-	          count += len_number(num);
-	          fun_number(num);
-                }
-              }
-             else
-             {
-             count += _putchar(*format);
-	     }
-      }
+	{
+	  format++;
+	  if (*format == 'c')
+	    {
+	      char c = va_arg(args, int);
+	      _putchar(c);
+	      count++;
+	    }
+	  else if (*format == 's')
+	    {
+	      char *str = va_arg(args, char *);
+	      count += print_string(str);
+	    }
+	  else if  (*format == '%')
+	    {
+
+	      count += _putchar(*format);
+	    }
+	  else if (*format == 'd' || *format == 'i')
+	    {
+	      {
+		int num = va_arg(args, int);
+		count += len_number(num);
+		fun_number(num);
+	      }
+	    }
+
+	  else if (*format == 'u')
+	  {
+	  unsigned int num = va_arg(args, unsigned int);
+	  count += len_unsigned_number(num);
+	  fun_unsigned_number(num);
+	  } 
+
+
+	  else if (*format == 'o')
+	  {
+	  unsigned int num = va_arg(args, unsigned int);
+	  count += len_octal_number(num);
+	  fun_octal_number(num);
+	  }
+
+	  else
+	    {
+	      count += _putchar(*format);
+	    }
+	}
       format++;
     }
   va_end(args);
