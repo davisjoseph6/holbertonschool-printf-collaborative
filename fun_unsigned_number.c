b697e583ad781a6afd550e79283ee413ba31be55
Davis <davisjoseph767@gmail.com>
2023-07-27 15:22:34 -0700

    reformatted fun unsigned number

--- fun_unsigned_number.c
+++ fun_unsigned_number.c
@@ -2,21 +2,31 @@
 
 int len_unsigned_number(unsigned int num)
 {
-  int count = 0;
-    if (num == 0)
-        return 1;
-       while (num > 0) {
-        count++;
-        num /= 10;
-    }
-    return count;
+	int count = 0;
+
+	if (num == 0)
+
+	return (1);
+
+	while (num > 0)
+	{
+		count++;
+		num /= 10;
+	}
+
+	return (count);
 }
+
 void fun_unsigned_number(unsigned int num)
 {
-    if (num >= 10) {
-        fun_unsigned_number(num / 10);
-        fun_unsigned_number(num % 10);
-    } else {
-        _putchar(num + '0');
-    }
+	if (num >= 10)
+	{
+		fun_unsigned_number(num / 10);
+		fun_unsigned_number(num % 10);
+	}
+
+	else
+	{
+		_putchar(num + '0');
+	}
 }
