c01b10bf7ec25b66b5ad17ab476b6cced34be6fe
Davis <davisjoseph767@gmail.com>
2023-07-27 08:19:05 -0700

    reformatted fun number

--- fun_number.c
+++ fun_number.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 int len_number(int num)
+
 {
 int i = 0;
 
 
 if (num == 0)
 return 1;
-  
 
 if (num == 0)
 return 1;
@@ -21,20 +21,23 @@ while (num > 0) {
 i++;
 num /= 10;
 }
-return i;
+ return (i);
 }
 void fun_number(int num)
 {
-if (num == -2147483648) {
+if (num == -2147483648)
+{
 _putchar('-');
 _putchar('2');
 fun_number(147483648);
 }
-else if (num < 0) {
+else if (num < 0)
+{
 _putchar('-');
 num = -num;
 }
-if (num >= 10) {
+if (num >= 10)
+{
 fun_number(num / 10);
 fun_number(num % 10);
 }
