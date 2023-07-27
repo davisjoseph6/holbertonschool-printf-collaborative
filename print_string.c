e94c9260b8fb4b936bc7151cabf9816041dd1a59
Davis <davisjoseph767@gmail.com>
2023-07-27 15:41:02 -0700

    reformatted print_string.c

--- print_string.c
+++ print_string.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
-int print_string (char *str)
+int print_string(char *str)
 {
-  int i=0;
+	int i = 0;
 
-  if (str == NULL)
-    {
-      /*_putchar('(');
-      _putchar('n');
-      _putchar('u');
-      _putchar('l');
-      _putchar('l');
-      _putchar(')');*/
-	    str = "(null)";
-    }
-  while (*str)
- 
-    {
-      i += _putchar(*str);
-      str++;
- 
-    }
-  return (i);
+	if (str == NULL)
+	{
+		/*
+		 * _putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		*/
+
+		str = "(null)";
+	}
+
+	while (*str)
+	{
+		i += _putchar(*str);
+		str++;
+
+	}
+
+	return (i);
 }
