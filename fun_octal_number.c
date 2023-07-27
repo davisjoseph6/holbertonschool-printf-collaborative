5ed0e2e7192ba5f252de8c78fe953bc4ad6077c3
Davis <davisjoseph767@gmail.com>
2023-07-27 03:07:08 -0700

    updated fun octal number

--- fun_octal_number.c
+++ fun_octal_number.c
@@ -1,16 +1,18 @@
 #include "main.h"
-int len_octal_number(unsigned int num) {
+int len_octal_number(unsigned int num)
+{
+int count = 0;
   if (num == 0)
     return 1;
-  int count = 0;
-  while (num > 0) {
+    while (num > 0) {
     count++;
     num /= 8;
   }
   return count;
 }
 
-void fun_octal_number(unsigned int num) {
+void fun_octal_number(unsigned int num)
+{
   if (num >= 8) {
     fun_octal_number(num / 8);
     fun_octal_number(num % 8);
