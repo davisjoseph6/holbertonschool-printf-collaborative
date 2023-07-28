66b7d9aa73945de84e4fee735ddc1c5d1650366b
Davis <davisjoseph767@gmail.com>
2023-07-28 03:55:58 -0700

    edited fun octal number header

--- fun_octal_number.c
+++ fun_octal_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * This function calculates the number of digits in the octal representation of an unsigned integer num.
+ * This function calculates the lenght of the number of digits in the octal representation of an unsigned integer num.
  * It takes an unsigned integer num as an input parameter.
- * The function returns an integer representing the number of digits in the octal representation of num.
+ * 
  * 
  * 
  */
@@ -26,10 +26,10 @@ int len_octal_number(unsigned int num)
 }
 
 /**
- * This function prints the octal representation of an unsigned integer num to the standard output (console).
- * It takes an unsigned integer num as an input parameter but doesn't return any value (void return type).
- * The function recursively calls itself to print each digit of the octal representation.
+ * This function prints the octal representation of an unsigned integer num.
  * 
+ * The function recursively calls itself to print each digit of the octal representation.
+ * Return: octal number
  * 
  */
 
